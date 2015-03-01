8654fc98a9ceef75b3a0f0a65abe2cefe7e0cff8
shaibruhis <shaibruhis@gmail.com>
2015-03-01 11:09:03 -0800

    checkout button now changes opacity based on cart amount

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -16,18 +16,12 @@ angular.module('starter.controllers', [])
       $rootScope.totalQuantity += num;
     }
 
-    // deal with displaying checkout if any card has quantity >= 1
     if ($rootScope.totalQuantity == 0) {
-      
+      document.getElementById('checkoutBarImg').style.opacity = ".8";
     }
-    else if ($rootScope.totalQuantity == 1) {
-
-    }
-    else if ($rootScope.totalQuantity == 2) {
-
-    }
-    else if ($rootScope.totalQuantity == 3) {
-
+    else {
+      document.getElementById('checkoutBar').style.opacity = "1.0";
+      document.getElementById('checkoutBarImg').style.opacity = "1.0";
     }
   }
 })
